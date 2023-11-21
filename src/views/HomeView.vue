ca8bdd8267378d68d16eea5bf1e321787eabac61
Kim Palao <kimfarhantpalao@gmail.com>
2023-11-21 22:47:38 +0800

    List audio files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+const audio = ref([]);
+
+const getAudio = async () => {
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}/audio/`);
+  audio.value = response.data.results;
+};
+
+onMounted(getAudio);
 </script>
 
 <template>
   <main>
+    <div v-for="file in audio" :key="file.id">
+      <h3>{{ file.name }}</h3>
+      <p>{{ file.description }}</p>
+    </div>
   </main>
 </template>
