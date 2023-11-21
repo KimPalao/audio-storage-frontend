5453d3ae1d9146a9446255440a1215e5e676f1c9
Kim Palao <kimfarhantpalao@gmail.com>
2023-11-21 22:47:38 +0800

    Add switch for nsfw toggle

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,37 @@
 <script setup lang="ts">
 import axios from 'axios';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 
 const audio = ref([]);
 
+const explicit = ref(false);
+
 const getAudio = async () => {
-  const response = await axios.get(`${import.meta.env.VITE_API_URL}/audio/`);
+  let url = `${import.meta.env.VITE_API_URL}/audio/`;
+  if (explicit.value) {
+    url += "?explicit";
+  }
+  const response = await axios.get(url);
   audio.value = response.data.results;
 };
 
 onMounted(getAudio);
+watch(explicit, getAudio);
 </script>
 
 <template>
   <main>
     <div class="container mt-4">
-      <div class="card" v-for="file in audio" :key="file.id">
+      <div class="row justify-content-end mt-4">
+        <div class="col-auto">
+          <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="showExplicit" v-model="explicit">
+            <label class="form-check-label" for="showExplicit">Show NSFW</label>
+          </div>
+
+        </div>
+      </div>
+      <div class="card my-4" v-for="file in audio" :key="file.id">
         <div class="card-body">
           <h5 class="card-title">{{ file.name }}</h5>
           <p class="card-text">{{ file.description }}</p>
@@ -26,3 +42,9 @@ onMounted(getAudio);
     </div>
   </main>
 </template>
+<style>
+.form-check-input:checked {
+  background-color: #fd0ddf57;
+  border-color: #fd0ddf57;
+}
+</style>
