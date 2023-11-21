00d83327b2e32a04fefc980e771445b8e887fb34
Kim Palao <kimfarhantpalao@gmail.com>
2023-11-21 22:47:38 +0800

    Add support for multiple files

--- src/components/AudioCard.vue
+++ src/components/AudioCard.vue
@@ -3,9 +3,15 @@
     <div class="card-body">
       <router-link class="card-title h5" v-if="link" :to="`/audio/${audio.slug}`">{{ audio.name }}</router-link>
       <h5 class="card-title" v-else>{{ audio.name }}</h5>
-      <p class="card-text">{{ audio.description }}</p>
-      <audio controls :src="audio.file">
-      </audio>
+      <p class="card-text" v-for="paragraph in paragraphs">
+        {{ paragraph }}
+      </p>
+      <div v-for="file in audio.files">
+        <hr>
+        <audio controls :src="file.file">
+        </audio>
+        <p>{{ file.notes }}</p>
+      </div>
     </div>
     <div class="card-footer text-end">
       <em>
@@ -16,6 +22,7 @@
 </template>
 <script setup lang="ts">
 import { DateTime } from 'luxon';
+import { computed } from 'vue';
 
 export interface Props {
   audio: any;
@@ -29,4 +36,8 @@ const props = withDefaults(defineProps<Props>(), {
 const format = (datetime: string) => {
   return DateTime.fromISO(datetime).toFormat('dd LLL y HH:mm:ss');
 };
+
+const paragraphs = computed(() => {
+  return props.audio.description.split("\r\n");
+});
 </script>
